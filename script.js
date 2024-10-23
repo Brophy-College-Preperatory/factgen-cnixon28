9c092224e6121fdb00ec46144b65615242bdddcf
cnixon28 <cnixon28@brophybroncos.org>
2024-10-23 21:19:42 +0000

    any comment

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ var factList = [
  "Fact 1",
  "Fact 2", 
  "Fact 3", 
- "Fact 4"];
+ "Fact 4"]; /*defines a list of facts to choose from*/
 
 var fact = document.getElementById("fact");
-var myBtn = document.getElementById("myBtn");
+var myBtn = document.getElementById("myBtn"); /*--defines different facts*/
 var count = 0;
 
-myBtn.addEventListener("click", displayFact);
+myBtn.addEventListener("click", displayFact); /*defines my button*/
 
 function displayFact(){
   fact.innerHTML = factList[count];
-  count++;
-  if (count == factList.length){
+  count++; /* adds one to the count variable*/
+  if (count == factList.length){ /*resets it to 0 if its equal to the fact length*/
     count = 0;
   }
 }
